d21bded060037ebb6287d440e64d43fa6442e9e0
lucienmaloney <lucienmaloney@student.lsmsa.edu>
2017-09-26 19:54:58 -0500

    Added dropdown for selecting thread

--- index.js
+++ index.js
@@ -29,26 +29,33 @@ const youtube_url = new URL(window.location.href);
 const v = youtube_url.searchParams.get("v");
 
 $.when(call1(v), call2(v), call3(v), call4(v)).done(function(r1, r2, r3, r4) {
-  const threads = r1[0].data.children.concat(r2[0].data.children).concat(r3[0].data.children).concat(r4[0].data.children);
+  $(document).ready(function() {
+    const threads = r1[0].data.children.concat(r2[0].data.children).concat(r3[0].data.children).concat(r4[0].data.children);
 
-  $("#ticket-shelf").after("<div id='reddit_comments'>Reddit Comments</div>");
-  $("#reddit_comments").append("<nav id='nav'></nav>");
-  $("#reddit_comments").append("<div id='title'></div>");
-  $("#reddit_comments").append("<div id='comments'></div>");
+    $("#ticket-shelf").after("<div id='reddit_comments'></div>");
+    $("#reddit_comments").append("<div id='top_bar'></div>");
+    $("#reddit_comments").append("<div id='nav'></div>");
+    $("#reddit_comments").append("<div id='title'></div>");
+    $("#reddit_comments").append("<div id='comments'></div>");
 
-  let grouped_data = {};
-  threads.forEach(function(thread) {
-    if(grouped_data[thread.data.subreddit]) {
-      grouped_data[thread.data.subreddit].push(thread);
-    } else {
-      grouped_data[thread.data.subreddit] = [thread];
-    }
+    const sorted_threads = threads.sort(function(a,b) {
+      const suba = a.data.subreddit;
+      const subb = b.data.subreddit;
+      return ((suba < subb) ? -1 : ((suba > subb) ? 1 : 0));
+    });
+    
+    let $thread_select = $("<select id='thread_select'></select>");
+
+    sorted_threads.forEach(function(thread) {
+      const t = thread.data;
+      const subreddit = "r/" + t.subreddit;
+      const forward = `${subreddit}, ${t.score}&#8679;, ${t.num_comments}&#128172;`;
+      const spaces = "&nbsp".repeat(52 - forward.length);
+      const sliced_title = t.title.length < 65 ? t.title : t.title.slice(0, 60) + "...";
+      const $opt = `<option value="${t.id}">${forward}${spaces} ${sliced_title}</option>`;
+      $thread_select.append($opt);
+    });
+
+    $("#reddit_comments > #nav").append($thread_select);
   });
-
-  //$.each(grouped_data, function(key, row) {
-  //  $("#nav").append();
-  //});
-
-  console.log(grouped_data);
-
 });
