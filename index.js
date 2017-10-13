ac6fbb99ac21dfa65241af7c636ce71e28cbb26f
lucienmaloney <lucienmaloney@student.lsmsa.edu>
2017-10-13 08:59:25 -0500

    Cleaned up code, optimized code, and added comments

--- index.js
+++ index.js
@@ -24,6 +24,11 @@ function call4(video_id) {
   });
 }
 
+function display_error_message() {
+  append_extension(false, "<h3 id='nothread'>Error Loading Reddit Content. ¯\\_(ツ)_/¯</h3>", "");
+  $("#reddit_comments > #nav").attr("display", "none");  
+}
+
 // url variable keeps track of current url so that if it changes we'll be able to tell
 let url = "";
 
@@ -34,52 +39,64 @@ function load_extension() {
 
   // Only load extension if v exists, which it won't on pages like the home page or settings
   if(v) {
-    $.when(call1(v), call2(v), call3(v), call4(v)).done(function(r1, r2, r3, r4) {
+    $.when(call1(v), call2(v), call3(v), call4(v)).then(function(r1, r2, r3, r4) {
       const threads = r1[0].data.children
                         .concat(r2[0].data.children)
                         .concat(r3[0].data.children)
                         .concat(r4[0].data.children);
 
-      // Get threads in alphabetical order so they're easier for the user to navigate:
-      const sorted_threads = threads.sort(function(a,b) {
-        const suba = a.data.subreddit.toLowerCase();
-        const subb = b.data.subreddit.toLowerCase();
-        return ((suba < subb) ? -1 : ((suba > subb) ? 1 : 0));
-      });
-      
-      let $thread_select = $("<select id='thread_select'></select>");
-
-      sorted_threads.forEach(function(thread) {
-        const t = thread.data;
-        const subreddit = "r/" + t.subreddit;
-        // &#8679; is an upvote symbol, &#128172; is a comment symbol
-        const forward = `${subreddit}, ${t.score}&#8679;, ${t.num_comments}&#128172;`;
-        // Add in a dynamic number of spaces so that all the video titles line up
-        const spaces = "&nbsp".repeat(52 - forward.length);
-        // Chop off titles that are too long to fit on screen:
-        const sliced_title = t.title.length < 65 ? t.title : t.title.slice(0, 60) + "...";
+      // If there is at least 1 thread:
+      if(threads.length) {
+        // Get threads in alphabetical order so they're easier for the user to navigate:
+        const sorted_threads = threads.sort(function(a,b) {
+          const suba = a.data.subreddit.toLowerCase();
+          const subb = b.data.subreddit.toLowerCase();
+          return ((suba < subb) ? -1 : ((suba > subb) ? 1 : 0));
+        });
         
-        const $opt = `<option 
-                        value="${t.permalink}" 
-                        title="${t.title.replace(/\"/g,'&quot;')}"
-                      >${forward}${spaces} ${sliced_title}</option>`;
-        $thread_select.append($opt);
-      });
+        let $thread_select = $("<select id='thread_select'></select>");
 
-      $thread_select.on('change', function(event) {
-        $("#reddit_comments > #comments").empty();
-        $("#reddit_comments > #title").empty().html("<h1>Loading Thread...</h1>");
-        setup_thread(event.target.value);
-      });
+        sorted_threads.forEach(function(thread) {
+          const t = thread.data;
+          const subreddit = "r/" + t.subreddit;
+          // &#8679; is an upvote symbol, &#128172; is a comment symbol
+          const forward = `${subreddit}, ${t.score}&#8679;, ${t.num_comments}&#128172;`;
+          // Add in a dynamic number of spaces so that all the video titles line up
+          const spaces = "&nbsp".repeat(52 - forward.length);
+          // Chop off titles that are too long to fit on screen:
+          const sliced_title = t.title.length < 65 ? t.title : t.title.slice(0, 60) + "...";
 
-      if(sorted_threads.length) {
+          const $opt = `<option 
+                          value="${t.permalink}" 
+                          title="${t.title.replace(/\"/g,'&quot;')}"
+                        >${forward}${spaces} ${sliced_title}</option>`;
+          $thread_select.append($opt);
+        });
+
+        $thread_select.on('change', function(event) {
+          $("#reddit_comments > #comments").empty();
+          $("#reddit_comments > #title").empty().html("<h1>Loading Thread...</h1>");
+          setup_thread(event.target.value);
+        });
         setup_thread(sorted_threads[0].data.permalink, $thread_select);
       } else {
         append_extension(false, "<h3 id='nothread'>No Threads Found</h3>", "");
         $("#reddit_comments > #nav").attr("display", "none");
       }
-    });
+    }).fail(display_error_message());
   }
+}
+
+function clean_reddit_content($content) {
+  // Reddit threads have a lot of html content that for this simplified extension
+  //   are unnecessary. The following is the list of all things that aren't needed.
+  const removables = `script, .cloneable, .panestack-title, .menuarea,
+                      .gold-wrap, .expand, .numchildren, .flat-list, .midcol,
+                      .domain, .flair, .linkflairlabel, .reportform,
+                      .expando-button, .score.likes, .score.dislikes,
+                      .userattrs, .gilded-icon, .morechildren, .parent`;
+  $content.find(removables).remove();
+  return $content;
 }
 
 function setup_thread(permalink, $thread_select) {
@@ -87,49 +104,33 @@ function setup_thread(permalink, $thread_select) {
     url: "https://www.reddit.com" + permalink,
 
     success: function(data) {
-      const $page = $(data);
-      $page.find("script").remove();
-      $page.find(".cloneable").remove();
-      $page.find(".panestack-title").remove();
-      $page.find(".menuarea").remove();
-      $page.find(".gold-wrap").remove();
-      $page.find(".expand").remove();
-      $page.find(".numchildren").remove();
-      $page.find(".flat-list").remove();
-      $page.find(".midcol").remove();
-      $page.find(".domain").remove();
-      $page.find(".flair").remove();
-      $page.find(".linkflairlabel").remove();
-      $page.find(".reportform").remove();
-      $page.find(".expando-button").remove();
-      $page.find(".score.likes").remove();
-      $page.find(".score.dislikes").remove();
-      $page.find(".userattrs").remove();
-      $page.find(".gilded-icon").parent().remove();
-      $page.find(".morechildren").remove();
-      $page.find(".parent").remove();
+      let $page = $(data);
+      // Make thread title link go to actual thread:
       $page.find("a.title").attr("href", "https://www.reddit.com" + permalink);
-      const $header = $page.find(".top-matter")[0].innerHTML;
-      const $comments = $page.find(".commentarea")[0].innerHTML;
+      $page = clean_reddit_content($page);
 
-      append_extension($thread_select, $header, $comments);
+      const header_html = $page.find(".top-matter")[0].innerHTML;
+      const comment_html = $page.find(".commentarea")[0].innerHTML;
+
+      append_extension($thread_select, header_html, comment_html);
     },
 
-    error: function(e) {
-
-    }
+    error: display_error_message()
   });
 }
 
 function click_thing(e) {
-  //console.log(e);
+  // Lots of elements in the reddit comments have onclick handlers that call a function "click_thing()"
+  // In order to prevent a console error about an undefined function, this empty function is inserted in 
+  //   a script on the page
 }
 
+// This function handles the clicking of the expand button so a user can hide the reddit extension
 function toggle_expand(elem) {
   document.querySelector("#reddit_comments > #nav").classList.toggle("reddit_hidden");
   document.querySelector("#reddit_comments > #title").classList.toggle("reddit_hidden");
   document.querySelector("#reddit_comments > #comments").classList.toggle("reddit_hidden");
-  //const expander = document.getElementById("h2 > #expand");
+  
   if(elem.innerHTML[1] === "-") {
     elem.innerHTML = "[+]";
   } else {
@@ -138,6 +139,7 @@ function toggle_expand(elem) {
 }
 
 function append_extension($thread_select, $header, $comments) {
+  // If extension not already appended, append it:
   if(!$("#reddit_comments").length) {
     $("#loading_roy").remove();
     $("#ticket-shelf").after("<div id='reddit_comments'></div>");
@@ -145,10 +147,19 @@ function append_extension($thread_select, $header, $comments) {
     $("#reddit_comments").append("<div id='nav'></div>");
     $("#reddit_comments").append("<div id='title'></div>");
     $("#reddit_comments").append("<div id='comments'></div>");
-    $("#reddit_comments > #top_bar").append(`<h2><a id="expand" href="javascript:void(0)" onclick="return toggle_expand(this)">[-]</a> Reddit On Youtube</h2><h2></h2>`);
+    const expander = `<h2>
+                        <a 
+                          id="expand" 
+                          href="javascript:void(0)" 
+                          onclick="return toggle_expand(this)"
+                        >[-]</a> 
+                      Reddit On Youtube</h2>`;
+    $("#reddit_comments > #top_bar").append(expander + "<h2></h2>");
+    // Append a short script to the page that so that clicks can be handled:
     $("#reddit_comments").append(`<script>${click_thing.toString() + toggle_expand.toString()}</script>`);
   }
 
+  // If passed a new thread dropdown, replace the old one
   if($thread_select) {
     const thread_number = $thread_select.children().length;
     const thread_text = thread_number === 1 ? "1 Thread" : thread_number + " Threads";
@@ -160,6 +171,10 @@ function append_extension($thread_select, $header, $comments) {
 
   $("#reddit_comments > #title").empty().append($header);
   $("#reddit_comments > #comments").empty().append($comments);
+
+  // Go through and update the links on the page to the proper base
+  // For example, there might be a link '/r/askreddit' that if we left alone would go to 'www.youtube.com/r/askreddit'
+  // So if a link starts with a forward slash we need to replace it with www.reddit.com/
   $("#reddit_comments > #comments").find("a:not(.author)").each(function() {
     const href = this.getAttribute("href");
     if(href[0] === "/") {
@@ -169,7 +184,8 @@ function append_extension($thread_select, $header, $comments) {
 
   if($("#reddit_comments > #nav > select").length) {
     const subreddit = $("#reddit_comments > #nav > select").find(":selected")[0].innerHTML.split(",")[0];
-    $("#reddit_comments > #title > .tagline").append(` to <a class="author" href="${'https://www.reddit.com/' + subreddit}">${subreddit}</a>`);
+    const sub_link = `<a class="author" href="${'https://www.reddit.com/' + subreddit}">${subreddit}</a>`;
+    $("#reddit_comments > #title > .tagline").append(" to " + sub_link);
   }
 }
 
